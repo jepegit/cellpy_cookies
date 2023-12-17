ec0c590ce3946fd386b76eb85d449c6fbaea3b94
jepegit <jepe@ife.no>
2023-12-17 12:34:24 +0100

    small fix on output txt

--- standard/hooks/pre_gen_project.py
+++ standard/hooks/pre_gen_project.py
@@ -20,28 +20,35 @@ if major == MIN_CELLPY_MAJOR and minor == MIN_CELLPY_MINOR:
     patch = cellpy_version.split(".")[3]
     if patch.startswith("0"):
         print()
-        print(80 * "=")
+        print(" ERROR ".center(80, "="))
+        print()
         print("  Cellpy Cookie says: 'OH NO!!!!'")
         print("  Cellpy Cookie says: 'Your version of cellpy is too old - aborting!'")
         print("  Cellpy Cookie says: 'Please update cellpy to the latest version'")
+        print()
         print("    $ python -m pip install cellpy --upgrade")
+        print()
         print("  Cellpy Cookie says: 'or use the cookie standard_1_0_0 instead!'")
+        print()
         print("    $ cellpy new --template starndard_1_0_0")
-
+        print()
         print(80 * "=")
+        print()
         sys.exit(1)
 
 # end of additional test
 
 if too_old:
     print()
-    print(80 * "=")
+    print(" ERROR ".center(80, "="))
+    print()
     print("  Cellpy Cookie says: 'OH NO!!!!'")
     print("  Cellpy Cookie says: 'Your version of cellpy is too old - aborting!'")
+    print()
     print(80 * "=")
     print()
     sys.exit(1)
 print("   Cellpy Cookie says: 'using cookie from the cellpy_cookies repository'")
-print(
-    f"   Cellpy Cookie says: 'setting up project in the following directory: {os.getcwd()}'"
-)
+print("   Cellpy Cookie says: 'setting up project in the following directory:'")
+print(f"      {os.getcwd()}'")
+print()
